f837d4322afc8b6b7ca7a2affe0a5fea2069d02a
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-02 08:54:21 -0400

    wording

--- reserved.js
+++ reserved.js
@@ -1,10 +1,12 @@
 'use strict';
 
 /**
- * It's necessary to separate these keys from config, so we don't
- * arbitrarily try to expand options keys into src-dest mappings.
- * These are keys that are typically passed to glob, globule, globby
- * expand-config, etc.
+ * "reserved" keys. These are keys that are necessary
+ * to separate from config, so we don't arbitrarily try
+ * to expand options keys into src-dest mappings.
+ *
+ * These are keys that are typically passed as options
+ * to glob, globule, globby expand-config, etc.
  */
 
 module.exports = [
